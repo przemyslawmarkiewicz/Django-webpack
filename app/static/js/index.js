e5e7c31d10b677ca54c440555790c5a40cf0189b
przemyslawmarkiewicz <przemyslawmarkiewicz97@gmail.com>
2020-08-12 13:12:16 +0200

    webpack dev-server

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -2,5 +2,5 @@ const name = document.getElementById('name');
 const counter = document.getElementById('counter');
 let count = 0;
 
-name.innerText = 'Przemek';
+name.innerText = 'Thomas';
 setInterval(() => counter.innerText = ++ count, 1000);
